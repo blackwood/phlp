8215cfa1dd19f570c5cb8121fc3fa797fa197d93
Miles Blackwood <milesblackwoodmusic@gmail.com>
2015-10-13 12:57:50 -0400

    Various semi-passing tests, interesting

--- index.php
+++ index.php
@@ -40,32 +40,15 @@ class Native {
 
 }
 
-// function def($name, $value) {
-// 	var_dump($name);
-// 	var_dump($value);
-// 	var_dump(Reducers::defined);
-// }
+function def($name, $value) {
+
+}
 
 function lisp($expression) {
 
-// come to an expression.
-
-// see if it the first value is a known function
-
-// if it is a function start looping over args
-
-// come to an expression
-
-// see if the first value is a known function
-
-// if it is not a function, this argument is an array, but, we should not evaluate, until we reach all inner args.
-
 	$reducers = Native::reducers();
-	// $defined  = Native::defined();
 
-	var_dump(array_key_exists($fn, $reducers));
-
-	if (function_exists($expression[0]) || array_key_exists($fn, $reducers)) {
+	if (function_exists($expression[0]) || array_key_exists($expression[0], $reducers)) {
 		$fn = $expression[0];
 	}
 
@@ -84,30 +67,19 @@ function lisp($expression) {
 	if (array_key_exists($fn, $reducers)) {
 		return array_reduce($args, $reducers[$fn]);
 	}
-
-	if (!$fn) {
-		return $args;
-	}
-
-	// var_dump(count($fn));
-	// var_dump($fn, $args);
-	// var_dump('--------');
-
-
-  // if (array_filter($args, 'is_array') === $args) {
-  // 	return call_user_func_array($fn, $args);
-  // }
-
-	// evaluate remaining list items
-	if ($fn) {
+  elseif ($fn && array_filter($args, 'is_array') === $args) {
+  	return call_user_func_array($fn, $args);
+  }
+	elseif ($fn) {
 		return array_map(function($arg) use ($fn) {
 			return call_user_func($fn, $arg);
 		}, $args);
+	} else {
+		return $args;
 	}
 
 }
 
 // lisp(['var_dump', ['+', 1, 2, 3, 4], ['.', 'foo', 'bar', 'baz']]);
-// lisp(['var_dump', 'foo',[2,3,4], ['array_merge',['+',[2],[3]],[4,5,6]]]);
-
-lisp(['var_dump', ['+',[2],[3]]]);
+// lisp(['var_dump', 'foo',[2,3,4], ['array_merge',[['+',9,10],2],[4,5,6]]]);
+// lisp(['var_dump', ['array_merge',[2],['array_merge',[3],[4]]]]);
